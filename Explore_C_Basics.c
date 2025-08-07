cddb609e303a751f08dd4bf0eca6b13452652094
MidoriyaKun <midoriyakun2437@gmail.com>
2025-08-07 10:07:58 +0530

    Create Explore_C_Basics.c
    Just the Problem Statement, Find Problems on the Internet Create a main body and different functions, the whole code may be huge but after each step take the compiled and checked functions into a header file so our final main code is clean and the all other huge chunks are hidden.
    Also understand the way every function is abstracted even if its on different location

---
+++ Explore_C_Basics.c
@@ -0,0 +1,29 @@
+//include header files
+
+
+//function prototypes
+
+//Main branch
+//Use the CLI arguments to run the program
+// Explore the return type and to retrieve the return type echo $
+//Explore the corner cases
+
+
+/*Create a simple Function to explore the possibilities of C
+Sub_function body
+ Try to implement a simple calc , 
+                    as well a recursive func, 
+                    Binary search , 
+                    array input, 
+                    Number compare, 
+                    Capitalise, 
+                    PARAMETR = N rotations reverse the string Maybe use the CLA in this scene for step or  circular roration  
+                    pointer practice
+                    some patterns
+                    mem space allotment
+                    linked list
+                    stack
+                    ques
+                    tries -  3d arrays???
+                    matrix
+  */
